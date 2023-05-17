c2535de6a1f8b220c8108e7a8a2a55d80bb9ecd4
Gabriel <gabriel.f2005@aluno.ifsc.edu.br>
2023-05-17 16:40:33 -0300

    Lista4
    aula 17 03

---
+++ ListaReforcoPOO/src/ex/MainProduto.java
@@ -0,0 +1,38 @@
+package ex;
+import java.util.ArrayList;
+
+public class MainProduto {
+
+	public static void main(String[] args) {
+		ArrayList<produto> folheto = new ArrayList<produto>();
+		produto a=new produto();
+		produto b=new produto();
+		produto c=new produto();
+		
+		a.setNome("Tody");
+		a.setCodBarras((long) 000000001);
+		a.setPreco(3.68);
+		
+		b.setNome("Nescau");
+		b.setCodBarras((long) 000000002);
+		b.setPreco(5.65);
+		
+		c.setNome("Nesquik");
+		c.setCodBarras((long) 000000003);
+		c.setPreco(12.69);
+		
+		folheto.add(a);
+		folheto.add(b);
+		folheto.add(c);
+		
+		int i=folheto.size();
+		for(int n=0;n<i;n++)
+		{
+			System.out.println(folheto.get(n).getNome());
+			System.out.println(folheto.get(n).getCodBarras());
+			System.out.println(folheto.get(n).getPreco());
+		}
+
+	}
+
+}
